566db3db472886bd17947d2f5dcb4ced9d03ffab
kmc97 <kmc97@duke.edu>
2018-04-02 10:32:55 -0400

    prints list onto website

--- hr-frontend/src/EmailResponse.js
+++ hr-frontend/src/EmailResponse.js
@@ -1,38 +1,71 @@
 import React from 'react';
 import TextField from 'material-ui/TextField';
-import MenuItem from 'material-ui/Menu/MenuItem';
-import { withStyles } from 'material-ui/styles';
-
+import Button from 'material-ui/Button';
+import axios from 'axios';
 
 class EmailResponse extends React.Component {
-	state = {
-		name: 'Kathleen'
+	constructor() {
+		super();
+		this.state = {
+			"usr_email": null, 
+			"data": [null],
+			"usr_hr": [null]
+		};
 	}
 
-	handleChange = name =>  event => {
-		this.setState({
-			[name]: event.target.value,
-		});
-	};
+	onNameTextFieldChange = (event) => {
+		this.setState({"nameTextField": event.target.value});
+	}
+
+	onButtonClick = (event) => {
+		console.log(this.state.nameTextField); // log the current nameTextField content
+	}
 
 
-
-
-
-
-
-
-
+	getData = () => {
+		var url_base = "http://vcm-3738.vm.duke.edu:5000/api/heart_rate/"
+		var email_lookup = this.state.nameTextField
+		var url_lookup = url_base.concat(email_lookup)
+		axios.get(url_lookup).then( (response) => {
+			console.log(response);
+			console.log(response.data.all_hr)
+			this.setState({
+				"data": response.data,
+				"usr_hr": response.data.all_hr
+			});
+		})	
+	}	
+		
 
 render() {
-    const { classes } = this.props;
+	return (
+	<div>	
+		<div>
+			<TextField
+				value={this.state.nameTextField}
+				onChange={this.onNameTextFieldChange}/>
+			<Button onClick={this.onButtonClick}>
+				Click HERE to enter data
+			</Button>
+		 
+		</div>
 
-    return (
-            <TextField
-            hintText="User Email"
-	    	/>   
-      
+		
+		<div>
+			<Button variant="raised" onClick={this.getData}>
+				Get Data
+			</Button>
+
+		<div>
+				
+			{this.state.data[0]}
+			{this.state.usr_hr}
+			</div>
+		</div>
+	</div>
 	);
-	}
+	}	
 }
+
+
 export default EmailResponse;
