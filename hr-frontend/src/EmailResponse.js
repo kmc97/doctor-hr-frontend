257c89533c8c3c7f405ddcdbb32f328807ba53a8
kmc97 <kmc97@duke.edu>
2018-04-02 20:43:02 -0400

    Table works, added max HR finder

--- hr-frontend/src/EmailResponse.js
+++ hr-frontend/src/EmailResponse.js
@@ -4,77 +4,95 @@ import Button from 'material-ui/Button';
 import axios from 'axios';
 import Table, { TableBody, TableCell, TableRow } from 'material-ui/Table';
 
-
+var styles = {
+    "dataStyle": {
+    "marginTop": "40px",
+    "marginLeft": "30%",
+    "marginBottom": "40px",
+    }
+}
 
 class EmailResponse extends React.Component {
-	constructor() {
-		super();
-		this.state = {
-			"tuple": [null],
-			"usr_hr": [null],
-			"timestamp":[null]
-		};
+    constructor() {
+        super();
+        this.state = {
+	    "tuple": [null],
+  	    "usr_hr": [null],
+	    "timestamp":[null],
+	    "max_hr": 0,
+	    };
 	}
 
-	onNameTextFieldChange = (event) => {
-		this.setState({"nameTextField": event.target.value});
+    onNameTextFieldChange = (event) => {
+        this.setState({"nameTextField": event.target.value});
 	}
 
-	onButtonClick = (event) => {
-		console.log(this.state.nameTextField); // log the current nameTextField content
+    onButtonClick = (event) => {
+        console.log(this.state.nameTextField); // log the current nameTextField content
 	}
 
+    getData = () => {
+        var url_base = "http://vcm-3738.vm.duke.edu:5000/api/heart_rate/"
+	var email_lookup = this.state.nameTextField
+	var url_lookup = url_base.concat(email_lookup)
+	var data = []
+	var final_data = []
 
-	getData = () => {
-		var url_base = "http://vcm-3738.vm.duke.edu:5000/api/heart_rate/"
-		var email_lookup = this.state.nameTextField
-		var url_lookup = url_base.concat(email_lookup)
-		axios.get(url_lookup).then( (response) => {
-			//console.log(response);
-			//console.log(response.data.all_hr)
-			//console.log(response.data.timestamp)
-			console.log([response.data.timestamp, response.data.all_hr])
-			this.setState({
-				"tuple": [response.data.all_hr, response.data.timestamp],
-				"usr_hr": response.data.all_hr,
-				"timestamp":response.data.timestamp		
-			});
-		})	
-	}			
+	axios.get(url_lookup).then( (response) => {
+      	for (let i=0; i < response.data.all_hr.length; i++) {
+	    data.push({
+	         "all_hr": response.data.all_hr[i],
+	         "timestamps": response.data.timestamp[i]  
+		});
+ 	}
+			
+	for (var i = 0; i<data.length; i++){
+	    var final_Data = ["Heart Rate (BPM): ", data[i].all_hr, "--time recorded at:  ", data[i].timestamps];
+	    final_data.push(final_Data)
+	    }
+
+   	    this.setState({
+	        "tuple": final_data,
+		"usr_hr": response.data.all_hr,
+		"timestamp":response.data.timestamp,
+		"max_hr":Math.max(...response.data.all_hr)		
+		});
+		console.log(this.state.tuple)
+	 })
+			
+    }			
 	
 
 render() {
-	return (
-	<div>	
-		<div>
-			<TextField
-				value={this.state.nameTextField}
-				onChange={this.onNameTextFieldChange}/>
-			<Button variant="raised" onClick={this.getData}>
-				Click HERE to retrieve data
-			</Button>
-		
-		</div>
-	
-		<Table>
-			<TableRow>
-			<TableCell>TimeStamp</TableCell>
-			<TableCell>HeartRates</TableCell>
-			</TableRow>
-			<TableBody>
-				{this.state.tuple.map(n =>{
-				
-				return(
-				<TableRow key={n}>
-				 <TableCell>{n}</TableCell>
-			
-				</TableRow>
+    return (
+        <div style={styles.dataStyle}>	
+  	    <div>
+	        type user email here: <TextField
+		    value={this.state.nameTextField}
+		        onChange={this.onNameTextFieldChange}/>
+	        <div> 
+ 	            <Button variant="raised" onClick={this.getData}>
+ 		        Click HERE to retrieve data
+		    </Button>
+    	        </div>
+	    </div>
+
+	    <Table>
+	        <TableRow>
+	            <TableCell> Heart Rate Table (max HR: {this.state.max_hr})</TableCell>
+		</TableRow>
+		<TableBody>
+  		    {this.state.tuple.map(n =>{		
+		        return(
+		  	    <TableRow key={n}>
+		   	        <TableCell>{n}</TableCell>
+			    </TableRow>
 			);
 	
-			})}
+		    })}
 	
-			</TableBody>	
-		</Table>	
+		</TableBody>	
+	    </Table>	
 	</div>
 	);
 	}	
